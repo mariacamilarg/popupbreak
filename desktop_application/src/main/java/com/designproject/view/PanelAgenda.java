b02ab9f05ac06cd73b3fe1e415ade61a71608d85
miriampunzi <mpunzi97@gmail.com>
2020-12-10 12:13:44 +0100

    Added button calendar sync

--- desktop_application/src/main/java/com/designproject/view/PanelAgenda.java
+++ desktop_application/src/main/java/com/designproject/view/PanelAgenda.java
@@ -29,12 +29,12 @@ public class PanelAgenda extends JPanel {
         this.interfacePopUpBreak = interfacePopUpBreak;
 
         // Set layout
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
         // Set margin
         this.setBorder(new EmptyBorder(20, 10, 10, 10));
 
-        // Initialize elements
+        // Initialize day commitments
         ArrayList<JCheckBox> dayCommitments = new ArrayList<>();
         dayCommitments.add(new JCheckBox("<html>9.00 - 10.00 <br> Team meeting</html>"));
         dayCommitments.add(new JCheckBox("<html>10.30 - 10.35 <br> Drink water</html>"));
@@ -43,16 +43,31 @@ public class PanelAgenda extends JPanel {
         dayCommitments.add(new JCheckBox("<html>16.00 - 16.30 <br> Healthy snack <br> Move excercise</html>"));
         dayCommitments.add(new JCheckBox("<html>17.30 - 17.35 <br> Eye break</html>"));
 
-        // Add elements to panel
+        // Add day commitments to panel
         for (int i = 0; i < dayCommitments.size(); i++) {
             dayCommitments.get(i).setVerticalTextPosition(SwingConstants.NORTH);
             add(dayCommitments.get(i));
             add(Box.createRigidArea(new Dimension(0, 10)));;
         }
 
+        // Initialize calendar button
+        ImageIcon calendarIcon = resizeIcon(
+                new ImageIcon("desktop_application/src/main/resources/icons/calendar-sync.png"),
+                25, 25);
+        JButton calendarButton = new JButton(calendarIcon);
+
+        // Add calendar button
+        add(Box.createVerticalGlue());
+        //calendarButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        add(calendarButton);
     }
 
     // -----------------------------------------------------------------
     // Methods
     // -----------------------------------------------------------------
+
+    public static ImageIcon resizeIcon(ImageIcon icon, int width, int height) {
+        Image image = icon.getImage().getScaledInstance(width, height,  java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(image);
+    }
 }
